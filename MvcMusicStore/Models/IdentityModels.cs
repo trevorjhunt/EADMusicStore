fd533d19f7a7337b9cc1e69080ffc4cbc9dce357
Trevor Hunt <trevor@hkc.ie>
2018-12-05 20:20:22 +0000

    Added shopping cart with ajax updates

--- MvcMusicStore/Models/IdentityModels.cs
+++ MvcMusicStore/Models/IdentityModels.cs
@@ -23,6 +23,10 @@ namespace MvcMusicStore.Models
         public DbSet<Album> Albums { get; set; }
         public DbSet<Genre> Genres { get; set; }
         public DbSet<Artist> Artists { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+
 
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
